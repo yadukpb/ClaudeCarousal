6f4ef88d23f66492d97ab0178fdccff924d70d77
yadukpb1 <yadukrishnapb1@gmail.com>
2024-12-18 03:46:00 +0530

    bacekndurk

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = 'http://localhost:4001';
-//export const BACKEND_URL = 'https://your-backend-url.com';
+//export const BACKEND_URL = 'http://localhost:4001';
+export const BACKEND_URL = 'https://clausecraftcarousal-backend.onrender.com';
